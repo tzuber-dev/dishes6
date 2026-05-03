62319b08d9924069eddf4a5ad8fadee6290454a5
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-05-03 01:10:40 -0500

    Update hello_test.cpp

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -5,6 +5,45 @@
 
 #include "../src/hello.hpp"
 
-TEST_CASE( "it returns Hello World" ) {
 
-}
+TEST_CASE("list does not crash upon reaching the maximum space") {
+ClassTwo listitem;
+
+ listitem.push(Dish("desc1"));
+ listitem.push(Dish("desc1"));
+ listitem.push(Dish("desc1"));
+ listitem.push(Dish("desc1"));
+
+ REQUIRE(listitem.size() == 4);
+
+
+ listitem.push(Dish("overlimit"));
+
+
+ REQUIRE(listitem.size() == 4);
+
+
+
+
+};
+
+TEST_CASE( "basic peek method works" ) {
+ ClassTwo listitem;
+ Dish item1("Item1");
+ Dish item2("Item2");
+
+
+ listitem.push(item1);
+ listitem.push(item2);
+
+
+ Dish verybeginningdish= listitem.peek();
+
+
+ REQUIRE(verybeginningdish.get_description() == "Item2");
+ REQUIRE(listitem.size() == 2);
+
+};
+
+
+
